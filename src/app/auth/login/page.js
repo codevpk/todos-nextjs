f0c99c64127ea96d7171d695c7fcfeedf437444a
CoDev <codevpk@gmail.com>
2025-11-02 02:48:46 +0500

    Some tweaks. Working start on auth

---
+++ src/app/auth/login/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <main className='flex-grow'>
+            <div className="container mx-auto px-4 py-15">
+                <h1 className="text-5xl text-center">Login</h1>
+            </div>
+        </main>
+    )
+}
+
+export default Login
--- src/components/root/Footer/index.js
+++ src/components/root/Footer/index.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="border-t border-gray-200 dark:border-gray-800 bg-gray-50 dark:bg-gray-950 text-center p-3 mt-10">
+        <footer className="border-t border-gray-200 dark:border-gray-800 bg-gray-50 dark:bg-gray-950 text-center px-4 py-3">
             <p className="text-sm text-gray-600 dark:text-gray-400">
                 &copy; {new Date().getFullYear()} Todos App by <a href="https://codevpk.com" target="_blank" rel="noopener noreferrer" className="font-semibold text-primary hover:underline">CoDev</a>. All Rights Reserved.
             </p>
